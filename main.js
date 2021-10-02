d941e480feb5a2440c2d262c77c507c663ec6101
SebastianBran <sebastianbranzapata@gmail.com>
2021-10-02 04:30:13 -0500

    add: added call to action links

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ document.getElementById("toggle-button").addEventListener("click", () => {
 
 let anchors = document.getElementsByTagName("a");
 for(let i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener("click", () => {
-        document.getElementById("menu").classList.toggle("active");
-    })
+    if (anchors[i].className === "a-navbar") {
+        anchors[i].addEventListener("click", () => {
+            document.getElementById("menu").classList.toggle("active");
+        });
+    }
 }
 //END NAVBAR
